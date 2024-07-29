9ad289ce594a115070f0318ca6736ea2b2cd04b4
Aigerim Beishenbekova <aigerimb@microsoft.com>
2024-07-29 17:48:38 +0600

    Copilot second commit

--- member.js
+++ member.js
@@ -1,7 +1,6 @@
-function skillMembers() {
+function skillsMember() {
   const members = ['John', 'Jane', 'Sam', 'Sue'];
   members.forEach((member) => {
     console.log(`${member} has a skill`);
   });
-  
 }
